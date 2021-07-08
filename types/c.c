eb9348a33de631500ce2a0c0fe6de9055c63e7fe
Шаталюк Евгения Олеговна <EOShatalyuk@sberbank.ru>
2021-07-09 01:37:17 +0300

    d

--- types/c.c
+++ types/c.c
@@ -1,45 +1,39 @@
 #include "../ft_printf.h"
 
+static int	c_w(char res, t_flags flags)
+{
+	int	sum_c;
+
+	sum_c = 0;
+	if ((flags.fl_z == 0 && flags.fl_m == 0)
+		|| (flags.fl_z != 0 && flags.fl_m != 0))
+	{
+		over_width(flags.val_w, 1);
+		sum_c = write_d_ch(res);
+	}
+	if (flags.fl_z != 0)
+	{
+		over_width_zero(flags.val_w, 1);
+		sum_c = write_d_ch(res);
+	}
+	if (flags.fl_m != 0)
+	{
+		sum_c = write_d_ch(res);
+		over_width(flags.val_w, 1);
+	}
+	return (sum_c);
+}
+
 int	c(t_flags flags, va_list args)
 {
 	char	res;
 	int		sum_c;
-	int		sum_over_width;
 
 	sum_c = 0;
-	sum_over_width = 0;
 	res = va_arg(args, int);
 	if (flags.fl_w == 0)
-	{
 		write(1, &res, 1);
-		sum_c = sum(1);
-	}
 	if (flags.fl_w != 0)
-	{
-		if (flags.fl_z == 0 && flags.fl_m == 0)
-		{
-			over_width(flags.val_w, 1);
-			write(1, &res, 1);
-			sum_c = sum(1);
-		}
-		if (flags.fl_z != 0)
-		{
-			over_width_zero(flags.val_w, 1);
-			write(1, &res, 1);
-			sum_c = sum(1);
-		}
-		if (flags.fl_m != 0)
-		{
-			write(1, &res, 1);
-			sum_c = sum(1);
-			sum_over_width = over_width(flags.val_w, 1);
-		}
-		if (flags.fl_z != 0 && flags.fl_m != 0)
-		{
-			over_width(flags.val_w, 1);
-			write(1, &res, 1);
-			sum_c = sum(1);
-		}
-	}
-	return (sum_c + sum_over_width);
+		c_w(res, flags);
+	return (sum_c);
 }
