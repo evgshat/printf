ca3f90aa29158cb63b2620f73df202af43a537a8
Шаталюк Евгения Олеговна <EOShatalyuk@sberbank.ru>
2021-07-13 17:39:42 +0300

    d

--- ft_printf.c
+++ ft_printf.c
@@ -2,14 +2,15 @@
 
 int	ft_printf(const char *format, ...)
 {
-	va_list				args;
-	int					res;
-	int					i;
+	va_list	args;
+	int		res;
+	int		i;
 
 	res = 0;
 	i = 0;
 	va_start(args, format);
 	res += write_before_perc((char *)format, &i);
 	res += processor(format, args, &i);
+	va_end(args);
 	return (res);
 }
